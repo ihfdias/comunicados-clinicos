3c4ab2353d5bad9e93f14a6e9a2d5d99121a5cca
Igor Dias <ihfdias.dev@gmail.com>
2025-07-10 15:28:19 -0300

    subindo para a hospedagem

--- render-build.sh
+++ render-build.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# Instalar dependências PHP
 composer install --no-dev --optimize-autoloader
 
-# Criar o banco de dados SQLite
 mkdir -p database
 touch database/database.sqlite
 
-# Copiar o .env de produção
 cp .env.production .env
 
-# Gerar a chave da aplicação
 php artisan key:generate
 
-# Rodar migrations (se tiver)
 php artisan migrate --force
