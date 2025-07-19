e43829ffe780ed32d430d566b702fa4e0611e359
Igor Dias <ihfdias.dev@gmail.com>
2025-07-19 11:30:31 -0300

    configurando o uso para casa

--- database/migrations/2025_07_10_155609_create_confirmacoes_leitura_table.php
+++ database/migrations/2025_07_10_155609_create_confirmacoes_leitura_table.php
@@ -7,17 +7,16 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create('confirmacao_leitura', function (Blueprint $table) {
+        Schema::create('confirmacao_leituras', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('comunicado_id')->constrained()->onDelete('cascade');
-            $table->timestamp('lido_em')->nullable();
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('confirmacoes_leitura');
+        Schema::dropIfExists('confirmacao_leituras');
     }
 };
