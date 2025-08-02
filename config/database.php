df96c1d323e8860b985139c0a212c995d141e35a
Igor Dias <ihfdias.dev@gmail.com>
2025-08-01 21:43:08 -0300

    correções database

--- config/database.php
+++ config/database.php
@@ -101,11 +101,11 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
         
-        'mongodb' => [
-        'driver' => 'mongodb',
-        'dsn' => env('DB_URI'),
-        'database' => env('DB_DATABASE', 'Users'),
-        ],
+       'mongodb' => [
+    'driver' => 'mongodb',
+    'dsn' => env('DB_URI'),
+    'database' => env('DB_DATABASE', 'Users'),
+    ],
     ],
 
     /*
